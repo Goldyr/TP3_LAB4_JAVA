dd09985bda9b6fe0e1233a475d3d9d95c8f78862
Seb-98 <sebastian98@outlook.com.ar>
2021-09-11 13:07:25 -0300

    V 0.2
    Cree el constructor con y sin parametros de Persona, la funcion verificarDNI, correji la herencia de DniInvalido a IOExeption

--- src/ejercicio1/Persona.java
+++ src/ejercicio1/Persona.java
@@ -4,8 +4,32 @@ public class Persona {
 	
 	private String nombre;
 	private String apellido;
-	private int DNI;
+	private String DNI;
 	
+	public Persona() {}
+	
+	public Persona(String nom, String ape, String dni) {
+		this.nombre=nom;
+		this.apellido=ape;
+		this.DNI=dni;
+	}
+	
+	public boolean VerificarDNIinvalido() throws DniInvalido{
+		int i;
+		Boolean HayLetra=false;
+		
+		for(i=0; i<DNI.length(); i++) {
+			if(Character.isLetter(DNI.charAt(i))==true) {
+				HayLetra=true;
+			}
+		}
+		
+		if(HayLetra==true) {
+			throw new DniInvalido();			
+		}
+		
+		return HayLetra;
+	}
 	
 	public String getNombre() {
 		return nombre;
@@ -19,11 +43,16 @@ public class Persona {
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	public int getDNI() {
+	public String getDNI() {
 		return DNI;
 	}
-	public void setDNI(int DNI) {
+	public void setDNI(String DNI) {
 		this.DNI = DNI;
+	}
+
+	@Override
+	public String toString() {
+		return "Nombre=" + nombre + ", Apellido=" + apellido + ", DNI=" + DNI;
 	}
 	
 	
--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -1,9 +1,18 @@
 package ejercicio1;
 
+import javax.swing.JOptionPane;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
+		try {
+			Persona per = new Persona("Matias", "Flori",JOptionPane.showInputDialog("Ingrese DNI: "));
+			per.VerificarDNIinvalido();
+
+		} catch (DniInvalido e) {
+			e.printStackTrace();
+		}
 		
 	}
 
